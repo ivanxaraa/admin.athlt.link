1d8dea4958672f84370135da3fb6880a426ab2ce
Ivan Xará <ivanmxara@gmail.com>
2024-04-28 23:54:04 +0100

    feat: SEO images

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 export const env = {
   NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  // website
+  PUBLIC_APP_LINK: process.env.PUBLIC_APP_LINK,
+  PUBLIC_APP_IMAGE: process.env.PUBLIC_APP_IMAGE
 };
 
 export const icon_size = {
