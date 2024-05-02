ed6e14b67cf8e409ac59fdb96e26bc091f5ea6a9
Ivan Xará <ivanmxara@gmail.com>
2024-05-03 00:11:39 +0100

    fix: middleware every page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import { supabase } from "./lib/supabase";
 import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-  // if(request.nextUrl.pathname === "/clubs")
-  // return NextResponse.redirect(new URL("/", request.url));
   const cookieStore = cookies();
   const accessToken = cookieStore.get("my-access-token")?.value;
   const refreshToken = cookieStore.get("my-refresh-token")?.value;
@@ -38,25 +36,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/",
+  matcher: "/:path*",
 };
-
-// // set session
-// const cookies = Object.fromEntries(
-//   document.cookie.split("; ").map((cookie) => cookie.split("="))
-// );
-// const accessToken = cookies["my-access-token"];
-// const refreshToken = cookies["my-refresh-token"];
-
-// if (accessToken && refreshToken) {
-//   await supabase.auth.setSession({
-//     access_token: accessToken,
-//     refresh_token: refreshToken,
-//   });
-// }
-
-// // get  user
-// const {
-//   data: { user },
-// } = await supabase.auth.getUser();
-// console.log(user);
