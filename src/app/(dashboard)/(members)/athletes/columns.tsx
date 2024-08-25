6494930425f07500dd63eddb36b0c708a13203cb
Ivan Xará <ivanmxara@gmail.com>
2024-08-25 22:11:55 +0100

    fix

--- src/app/(dashboard)/(members)/athletes/columns.tsx
+++ src/app/(dashboard)/(members)/athletes/columns.tsx
@@ -12,7 +12,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Checkbox } from "@/components/ui/checkbox";
 
-export const columns = [
+export const columns = (caller?: { handleActivals: Function }) => [
   {
     id: "select",
     header: ({ table }: any) => (
--- src/app/(dashboard)/(members)/athletes/page.tsx
+++ src/app/(dashboard)/(members)/athletes/page.tsx
@@ -1,15 +1,39 @@
+"use client";
+
 import { DataTable } from "@/components/ui/data-table";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { columns } from "./columns";
 import Heading1 from "@/components/ui/heading-1";
 import { athletesControl } from "@/controllers/athletesControl";
-import { columns } from "./columns";
+import { useRouter } from "next/navigation";
+import { app } from "@/utils/constants";
 
-const Page = async () => {
-  const athletes = await athletesControl.get();
+const Page = () => {
+  const router = useRouter();
+  const [athletes, setAthletes] = useState<any>([]);
+
+  const getAthletes = async () => {
+    const data = await athletesControl.get();
+    console.log(data);
+
+    setAthletes(data);
+  };
+
+  useEffect(() => {
+    getAthletes();
+  }, []);
+
   return (
     <>
       <Heading1>Athletes</Heading1>
-      <DataTable columns={columns} data={athletes} hide={{ columns: true }} />
+      <DataTable
+        columns={columns()}
+        data={athletes}
+        hide={{ columns: true }}
+        rowClick={(row: any) =>
+          router.push(`${app.website_url}/${row.username}`)
+        }
+      />
     </>
   );
 };
