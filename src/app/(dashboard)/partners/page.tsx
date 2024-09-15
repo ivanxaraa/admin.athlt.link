d94b272e7cfd910618258ceca6b7da58df9006d1
Ivan Xará <ivanmxara@gmail.com>
2024-09-15 21:02:01 +0100

    feat: drag partners

--- src/app/(dashboard)/partners/page.tsx
+++ src/app/(dashboard)/partners/page.tsx
@@ -1,51 +1,123 @@
 "use client";
 
-import { DataTable } from "@/components/ui/data-table";
 import React, { useEffect, useState } from "react";
-import { columns } from "./columns";
-import Heading1 from "@/components/ui/heading-1";
-import { clubsControl } from "@/controllers/clubsControl";
 import { useRouter } from "next/navigation";
-import { copy } from "@/utils/copy";
-import { ClipboardPlus } from "lucide-react";
 import { supabase } from "@/lib/supabase";
+import { GripVertical, Plus } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Heading1 from "@/components/ui/heading-1";
+import { Reorder, useDragControls } from "framer-motion";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { toast } from "sonner";
+import { Switch } from "@/components/ui/switch";
+
+const Item = ({ item }: any) => {
+  const router = useRouter();
+  const controls = useDragControls();
+
+  return (
+    <Reorder.Item
+      id={item.id}
+      key={item.id}
+      value={item}
+      dragListener={false}
+      dragControls={controls}
+    >
+      <button className="flex items-center gap-4 w-full justify-center bg-white border rounded-lg p-4">
+        <GripVertical onPointerDown={(e) => controls.start(e)} />
+        <div
+          className="flex gap-4 w-full"
+          onClick={() => router.push(`/partners/${item.id}`)}
+        >
+          <Avatar className="size-8">
+            <AvatarImage src={item.image} />
+            <AvatarFallback></AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <span className="font-semibold text-sm text-left">{item.name}</span>
+            <span className="text-left text-xs">{item.description}</span>
+          </div>
+        </div>
+        <div className="ml-auto flex items-center">
+          <Switch
+            onClick={(e) => e.stopPropagation()}
+            onCheckedChange={async (val) => {
+              try {
+                const { error } = await supabase
+                  .from("partners")
+                  .update({ status: val })
+                  .eq("id", item.id);
+
+                if (error) throw error;
+
+                toast.success(`Partner modified successfully!`);
+              } catch (err) {
+                toast.error("Error!");
+              }
+            }}
+            defaultChecked={item.status}
+          />
+        </div>
+      </button>
+    </Reorder.Item>
+  );
+};
 
 const Page = () => {
-  const router = useRouter();
-  const [data, setData] = useState<any>([]);
-
-  const fetch = async () => {
-    const { data } = await supabase.from("partners").select();
-    setData(data);
-  };
-
-  const actions = {
-    view: (item: any) => {
-      router.push(`partners/${item.name}`);
-    },
-  };
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
+    const fetch = async () => {
+      const { data } = await supabase.from("partners").select().order("order");
+      setData(data || []);
+    };
+
     fetch();
   }, []);
+
+  const handleReorder = async (newOrder: any[]) => {
+    try {
+      setData(newOrder);
+
+      const updates = newOrder.map((item: any, index: number) => ({
+        ...item,
+        order: index,
+      }));
+
+      const { error } = await supabase.from("partners").upsert(updates);
+      if (error) throw error;
+    } catch (err) {
+      toast.error("Error");
+    }
+  };
 
   return (
     <>
       <Heading1>Partners</Heading1>
-      <DataTable
-        data={data}
-        hide={{ columns: true }}
-        columns={columns({
-          actions: [{ label: "View", click: actions.view }],
-        })}
-        buttons={[
-          {
-            label: "Create Partner",
-            click: () => router.push("partners/create"),
-          },
-        ]}
-        rowClick={(row: any) => router.push(`partners/${row.id}`)}
-      />
+      <div className="w-full">
+        <div className="flex items-center py-4 gap-4">
+          <Input placeholder="Filter by name..." className="max-w-sm" />
+          <Link className="ml-auto" href="/partners/create">
+            <Button className="ml-auto">
+              <Plus className="size-4" />
+            </Button>
+          </Link>
+        </div>
+
+        {/* Reorder Group */}
+        <Reorder.Group
+          axis="y"
+          values={data}
+          onReorder={handleReorder}
+          className="flex flex-col gap-2"
+        >
+          {data.map((item, index) => (
+            <Item key={item.id} item={item} />
+          ))}
+        </Reorder.Group>
+      </div>
     </>
   );
 };
--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -1,55 +1,44 @@
 import { generic } from "./generic";
 
+// https://chatgpt.com/c/66e6f377-2e54-8013-a2d0-02bccb5a3db3
 export const getTextInvitation = {
   paid: (club: any, team: any, code: string) => {
     return `Hey, you’ve been invited to join ${club.name} ${
       team.name
-    } on ATHLT, the best platform for live recruiting.
+    } on ATHLT, the all in one profile page for athletes.
 
-Use this link to download the app and create your free profile:
-https://athlt.link/download
-
-Access your Dashboard - TeamLink and enter the code:
-*${code}*
+Team code: ${code}
 
 Connect to your team costs only $${generic.number.toDecimal(
       club.fee
     )} for the full season.
 
-For more information about ATHLT:
+Use this link to download the app.
 www.athlt.link`;
   },
   free: (club: any, team: any, code: string) => {
-    return `Hey, you’ve been invited to join ${club.name} ${team.name} on ATHLT, the best platform for live recruiting.
+    return `Hey, you’ve been invited to join ${club.name} ${team.name} on ATHLT, the all in one profile page for athletes.
 
-Use this link to download the app and create your free profile:
-https://athlt.link/download
+Team code: ${code}
 
-Access your Dashboard - TeamLink and enter the code:
-*${code}*
+Connect to your team it's free for the full season.
 
-Connect to your team it’s free for the full season.
-
-For more information about ATHLT:
+Use this link to download the app.
 www.athlt.link`;
   },
   freeVerification: (club: any, team: any, code: string) => {
     return `Hey, you’ve been invited to join ${club.name} ${
       team.name
-    } on ATHLT, the best platform for live recruiting.
+    } on ATHLT, the all in one profile page for athletes.
 
-Use this link to download the app and create your free profile:
-https://athlt.link/download
-
-Access your Dashboard - TeamLink and enter the code:
-*${code}*
+Team code: ${code}
 
 Connect to your team costs only $${generic.number.toDecimal(
       club.fee
     )} for the full season.
 As a special offer you can verify your ID for free.
 
-For more information about ATHLT:
+Use this link to download the app.
 www.athlt.link`;
   },
 };
